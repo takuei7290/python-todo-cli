67a4b429dd6bdea10ab5133b4bad4f809bb03569
takuei <skyhigh72901@gmail.com>
2026-09-20 10:12:51 +0900

    feat: タスク一覧表示(list)機能を実装

--- todo.py
+++ todo.py
@@ -22,3 +22,23 @@ if sys.argv[1]=="add":
     tasks.append(new_task)
     with open("tasks.json", "w", encoding="utf-8") as f:
         json.dump(tasks, f, ensure_ascii=False)
+
+elif sys.argv[1]=="list":
+
+    if os.path.exists("tasks.json"):
+            with open("tasks.json", "r", encoding="utf-8") as f:
+                tasks = json.load(f)
+
+    else:
+            tasks=[]           
+
+
+    if len(tasks)==0:
+        print("タスクはありません")
+
+    else:
+        for task in tasks:
+            if task["done"]==True:
+                print(str(task["id"]) + ". " + task["content"] + " [完了]")
+            else:
+                print(str(task["id"]) + ". " + task["content"] + " [未完了]")
