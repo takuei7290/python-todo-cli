c648310f47f9c294affaa9adf8e2d5ab6e413b22
takuei <skyhigh72901@gmail.com>
2026-09-23 19:40:40 +0900

    refactor: add/list/done/deleteの処理をそれぞれ関数に分割

--- todo.py
+++ todo.py
@@ -24,33 +24,7 @@ def save_tasks(tasks):
     with open("tasks.json", "w", encoding="utf-8") as f:
         json.dump(tasks, f, ensure_ascii=False)
 
-
-if len(sys.argv)<2:
-    print("コマンドがありません。add / list / done / delete のいずれかを指定してください")
-    sys.exit()
-
-
-if sys.argv[1]=="add":
-
-    tasks=load_tasks()
-
-    if len(sys.argv)<3 or " ".join(sys.argv[2:]).strip()=="":
-        print("内容を入力してください")
-
-    else:
-        new_task={
-            "id": max((task["id"] for task in tasks) , default=0)+1 ,
-            "content": " ".join(sys.argv[2:]) ,
-            "done": False ,
-            "created_at": str(datetime.now())
-        }
-
-        tasks.append(new_task)
-        print("追加しました")
-        save_tasks(tasks)
-
-elif sys.argv[1]=="list":
-
+def list_tasks():
     tasks=load_tasks()         
 
     if len(tasks)==0:
@@ -63,49 +37,81 @@ elif sys.argv[1]=="list":
             else:
                 print(str(task["id"]) + ". " + task["content"] + " [未完了]")
 
-elif sys.argv[1]=="done":
+def add_task(content):
+    tasks=load_tasks()
+    
+    if content.strip()=="":
+            print("内容を入力してください")
+    
+    else:
+        new_task={
+            "id": max((task["id"] for task in tasks) , default=0)+1 ,
+            "content": content ,
+            "done": False ,
+            "created_at": str(datetime.now())
+        }
+    
+        tasks.append(new_task)
+        print("追加しました")
+        save_tasks(tasks)
 
+def done_task(task_id):
     tasks=load_tasks()
 
-    if len(sys.argv)<3:
-            print("idを入力してください")
+    found=False
+    for task in tasks:
+        if str(task["id"])==task_id:
+            task["done"]=True
+            save_tasks(tasks)
+            print("完了にしました")
+            found=True
 
-    else:
-        found=False
-        for task in tasks:
-            if str(task["id"])==sys.argv[2]:
-                task["done"]=True
-                save_tasks(tasks)
-                print("完了にしました")
-                found=True
+    if found==False:
+        print("該当するタスクがありません")     
 
-        if found==False:
-            print("該当するタスクがありません")
-
-elif sys.argv[1]=="delete":
-
+def delete_task(task_id):
     tasks=load_tasks() 
 
     new_tasks=[] 
     found=False
-
-    if len(sys.argv)<3:
-                print("idを入力してください")
-
-    else:            
-        for task in tasks:
-            if str(task["id"])==sys.argv[2]:
-                found=True
-
-            else:
-                new_tasks.append(task)
-
-        if found==True:
-            save_tasks(new_tasks)
-            print("削除しました")
+    for task in tasks:
+        if str(task["id"])==task_id:
+            found=True
 
         else:
-            print("該当するタスクがありません")
+            new_tasks.append(task)
+
+    if found==True:
+        save_tasks(new_tasks)
+        print("削除しました")
+
+    else:
+        print("該当するタスクがありません")
+   
+
+if len(sys.argv)<2:
+    print("コマンドがありません。add / list / done / delete のいずれかを指定してください")
+    sys.exit()
+
+if sys.argv[1]=="add":
+    add_task(" ".join(sys.argv[2:]))
+
+elif sys.argv[1]=="list":
+    list_tasks()
+
+elif sys.argv[1]=="done":
+    if len(sys.argv)<3:
+        print("idを入力してください")
+
+    else:
+        done_task(sys.argv[2])
+
+elif sys.argv[1]=="delete":
+    if len(sys.argv)<3:
+        print("idを入力してください")
+
+    else:            
+        delete_task(sys.argv[2])
 
 else:
     print("そのコマンドは無効です")
